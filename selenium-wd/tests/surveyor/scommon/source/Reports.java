1a980a04ec0261f34d08c7d7a2ef36ea9d06057a
Zhenting Lu <zlu@picarro.com>
2014-08-20 14:08:52 -0700

    fixing bug: specify the tag for report generation

--- selenium-wd/tests/surveyor/scommon/source/Reports.java
+++ selenium-wd/tests/surveyor/scommon/source/Reports.java
@@ -21,11 +21,8 @@ public interface Reports {
 	public static final String SWLAT = "37.32908214394206";
 	public static final String SWLON = "-122.12385177612305";
 	
-//	public static final String SURVEYORUNIT = "Picarro - Default - pdFDDS2037a";
-//	public static final String TAG = "dmcs1-sqa01";
-	
-	public static final String SURVEYORUNIT = "Picarro - Default - Surveyor Unit 1";
-	public static final String TAG = "RB-S1-2014-08-09-1059";
+	public static final String SURVEYORUNIT = "Picarro - Default - pdFDDS2037a";
+	public static final String TAG = "dmcs1-sqa01";
 	
 	public static final String STARTDATE = "01/01/2014";
 	public static final String ENDDATE = "08/14/2014";
