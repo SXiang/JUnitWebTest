9d680600474882911b137dffd98fd663c51b53d8
pvmahajan <pmahajan@picarro.com>
2015-08-14 12:24:31 -0500

    Fixed test case numbers

--- selenium-wd/tests/surveyor/regression/source/ReferenceGasReportsPageTest.java
+++ selenium-wd/tests/surveyor/regression/source/ReferenceGasReportsPageTest.java
@@ -14,6 +14,7 @@ import static surveyor.scommon.source.SurveyorConstants.SQACUSSU;
 import static surveyor.scommon.source.SurveyorConstants.TIMEZONEPT;
 import static surveyor.scommon.source.SurveyorConstants.TIMEZONEMT;
 import static surveyor.scommon.source.SurveyorConstants.TIMEZONECT;
+import static surveyor.scommon.source.SurveyorConstants.TIMEZONEET;
 import static surveyor.scommon.source.SurveyorConstants.USERPASSWORD;
 import static surveyor.scommon.source.SurveyorConstants.SQACUSUA;
 import static surveyor.scommon.source.SurveyorConstants.SQACUSLOCSUR;
@@ -45,15 +46,15 @@ public class ReferenceGasReportsPageTest extends SurveyorBaseTest {
 	}
 
 	/**
-	 * Test Case ID: RPT014 Test Description: Generate Generate Reference Gas
+	 * Test Case ID: TC159 Test Description: Generate Generate Reference Gas
 	 * Capture Report as Administrator
 	 * 
 	 */
 	@Test
-	public void RPT014() {
-		String rptTitle = "RPT014 Report" + testSetup.getRandomNumber();
+	public void TC159_GenerateRefGasRpt_PicarroAdmin() {
+		String rptTitle = "TC159 Report" + testSetup.getRandomNumber();
 		System.out
-				.format("\nRunning RPT014 Test Description: Generate Reference Gas Capture Report as Administrator, %s\n",
+				.format("\nRunning TC159 Test Description: Generate Reference Gas Capture Report as Administrator, %s\n",
 						rptTitle);
 
 		String surveyorUnit = SQACUS + " - " + SQACUSLOC + " - " + SQACUSLOCSUR;
@@ -81,30 +82,29 @@ public class ReferenceGasReportsPageTest extends SurveyorBaseTest {
 				testSetup.getLoginPwd());
 		referenceGasReportsPage.open();
 
-		referenceGasReportsPage.addNewPDReport(rptTitle, TIMEZONEPT,
-				surveyorUnit, startDate, endDate);
+		referenceGasReportsPage.addNewReport(rptTitle, TIMEZONEET,
+				surveyorUnit, startDate, endDate, 7, 0);
 
 		testSetup.slowdownInSeconds(testSetup.getSlowdownInSeconds());
 
 		if ((referenceGasReportsPage.checkActionStatus(rptTitle, PICDFADMIN))) {
 			assertTrue(referenceGasReportsPage.findReport(rptTitle, PICDFADMIN));
-			assertTrue("Report not downloaded successfully!",
-					referenceGasReportsPage.validatePdfFiles(rptTitle,
+			assertTrue(referenceGasReportsPage.validatePdfFiles(rptTitle,
 							testSetup.getDownloadPath()));
 		} else
-			fail("\nTestcase RPT014 failed.\n");
+			fail("\nTestcase TC159 failed.\n");
 
 		referenceGasReportsPage.open();
 		referenceGasReportsPage.logout();
 	}
 
 	/**
-	 * Test Case ID: RPT015B Test Description: Pagination - 10,25,50 and 100
+	 * Test Case ID: TC162 Test Description: Pagination - 10,25,50 and 100
 	 * Reports selection on reference gas report screen
 	 * 
 	 */
 	@Test
-	public void RPT015B() {
+	public void TC162_RefGasRpt_Pagination() {
 		System.out
 				.format("\nRunning RPT015A: Pagination - 10,25,50 and 100 Reports selection on reference gas report screen");
 
@@ -126,15 +126,15 @@ public class ReferenceGasReportsPageTest extends SurveyorBaseTest {
 	}
 
 	/**
-	 * Test Case ID: RPT029 Test Description: Generate Reference Gas Capture
+	 * Test Case ID: TC179 Test Description: Generate Reference Gas Capture
 	 * Report as customer admin
 	 * 
 	 */
 	@Test
-	public void RPT029() {
-		String rptTitle = "RPT029 Report" + testSetup.getRandomNumber();
+	public void TC179_GenerateRefGasRpt_CustAdmin() {
+		String rptTitle = "TC179 Report" + testSetup.getRandomNumber();
 		System.out
-				.format("\nRunning RPT029 Test Description: Generate Reference Gas Capture Report as customer admin, %s\n",
+				.format("\nRunning TC179 Test Description: Generate Reference Gas Capture Report as customer admin, %s\n",
 						rptTitle);
 
 		String surveyorUnit = SQACUS + " - " + SQACUSLOC + " - " + SQACUSLOCSUR;
@@ -172,21 +172,21 @@ public class ReferenceGasReportsPageTest extends SurveyorBaseTest {
 			assertTrue(referenceGasReportsPage.validatePdfFiles(rptTitle,
 					testSetup.getDownloadPath()));
 		} else
-			fail("\nTestcase RPT029 failed. Report not downloaded successfully!\n");
+			fail("\nTestcase TC179 failed. Report not downloaded successfully!\n");
 
 		referenceGasReportsPage.open();
 		referenceGasReportsPage.logout();
 	}
 
 	/**
-	 * Test Case ID: RPT035B Test Description: Click on Cancel button present on
+	 * Test Case ID: TC187 Test Description: Click on Cancel button present on
 	 * reference gas report screen
 	 * 
 	 */
 	@Test
-	public void RPT035B() {
+	public void TC187_RefGasRptScreen_CancelBtn() {
 		System.out
-				.format("\nRunning RPT035B: Click on Cancel button present on reference gas report screen\n");
+				.format("\nRunning TC187: Click on Cancel button present on reference gas report screen\n");
 
 		referenceGasReportsPage.login(testSetup.getLoginUser(),
 				testSetup.getLoginPwd());
@@ -199,45 +199,31 @@ public class ReferenceGasReportsPageTest extends SurveyorBaseTest {
 	}
 
 	/**
-	 * Test Case ID: RPT043 Test Description: Generate Generate Reference Gas
+	 * Test Case ID: TC196 Test Description: Generate Generate Reference Gas
 	 * Capture Report for single day
 	 * 
 	 */
-	@Test
-	public void RPT043() {
-		String rptTitle = "RPT043 Report" + testSetup.getRandomNumber();
+//	@Test
+// Need to check how to select 6/29 start date
+	public void TC196_GenerateRefGasRpt_SingleDay() {
+		String rptTitle = "TC196 Report" + testSetup.getRandomNumber();
+		String startDate = "28";
+		String endDate = "29";
+		
 		System.out
-				.format("\nRunning RPT043 Test Description: Generate Reference Gas Capture Report for single day, %s\n",
+				.format("\nRunning TC196 Test Description: Generate Reference Gas Capture Report for single day, %s\n",
 						rptTitle);
 
 		String surveyorUnit = SQACUS + " - " + SQACUSLOC + "0" + " - "
 				+ SQACUSLOC0SUR;
 		System.out.println(surveyorUnit);
 
-		DateFormat dateFormat = new SimpleDateFormat("dd");
-		Date date = new Date();
-		Calendar cal = Calendar.getInstance();
-		cal.add(Calendar.DATE, -1);
-		String startDate = dateFormat.format(cal.getTime());
-		System.out.println("Start Date : " + startDate);
-		if (startDate.startsWith("0")) {
-			startDate = startDate.replaceFirst("0*", "");
-			System.out.println("New Start Date : " + startDate);
-		}
-		date = new Date();
-		String endDate = dateFormat.format(date);
-		System.out.println("End Date : " + endDate);
-		if (endDate.startsWith("0")) {
-			endDate = endDate.replaceFirst("0*", "");
-			System.out.println("New End Date : " + endDate);
-		}
-
 		referenceGasReportsPage.login(testSetup.getLoginUser(),
 				testSetup.getLoginPwd());
 		referenceGasReportsPage.open();
 
-		referenceGasReportsPage.addNewPDReport(rptTitle, TIMEZONEPT,
-				surveyorUnit, startDate, endDate);
+		referenceGasReportsPage.addNewReport(rptTitle, TIMEZONEPT,
+				surveyorUnit, startDate, endDate, 6, 6);
 
 		testSetup.slowdownInSeconds(testSetup.getSlowdownInSeconds());
 
@@ -246,19 +232,19 @@ public class ReferenceGasReportsPageTest extends SurveyorBaseTest {
 			assertTrue(referenceGasReportsPage.validatePdfFiles(rptTitle,
 							testSetup.getDownloadPath()));
 		} else
-			fail("\nTestcase RPT043 failed.\n");
+			fail("\nTestcase TC196 failed.\n");
 
 		referenceGasReportsPage.open();
 		referenceGasReportsPage.logout();
 	}
 
 	/**
-	 * Test Case ID: RPT029 Test Description: Generate Reference Gas Capture
+	 * Test Case ID: TC515 Test Description: Generate Reference Gas Capture
 	 * Report as customer supervisor
 	 * 
 	 */
 	@Test
-	public void TC515_GenerateRefGasReport_CustomerSupervisor() {
+	public void TC515_GenerateRefGasRpt_CustSupervisor() {
 		String rptTitle = "TC515 Report" + testSetup.getRandomNumber();
 		System.out
 				.format("\nRunning TC515 Test Description: Generate Reference Gas Capture Report as customer supervisor, %s\n",
@@ -300,7 +286,7 @@ public class ReferenceGasReportsPageTest extends SurveyorBaseTest {
 					referenceGasReportsPage.validatePdfFiles(rptTitle,
 							testSetup.getDownloadPath()));
 		} else
-			fail("\nTestcase RPT515 failed.Report failed to download\n");
+			fail("\nTestcase RPT515 failed. Either Report failed to generate or download!\n");
 
 		referenceGasReportsPage.open();
 		referenceGasReportsPage.logout();
