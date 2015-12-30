b8060db7b9a1e9943adcde2340078f95f2d4cdbe
picarro-spulikkal <spulikkal@picarro.com>
2015-12-29 17:28:32 -0800

    Updated setup simulator script to include placeholder for working directory

--- selenium-wd/lib/SetupSimulatorPreReqs.cmd
+++ selenium-wd/lib/SetupSimulatorPreReqs.cmd
@@ -1,2 +1,2 @@
-Powershell -command "& {Set-ExecutionPolicy Unrestricted -Force;Unblock-File -Path C:\Repositories\surveyor-qa\selenium-wd\lib\SetupSimulatorPreReqs.ps1;C:\Repositories\surveyor-qa\selenium-wd\lib\SetupSimulatorPreReqs.ps1 -isRunningLocally:$false; exit;}"
+Powershell -command "& {Set-ExecutionPolicy Unrestricted -Force;Unblock-File -Path %CI_WORKING_DIR%\selenium-wd\lib\SetupSimulatorPreReqs.ps1;%CI_WORKING_DIR%\selenium-wd\lib\SetupSimulatorPreReqs.ps1 -isRunningLocally:$false; exit;}"
 exit
