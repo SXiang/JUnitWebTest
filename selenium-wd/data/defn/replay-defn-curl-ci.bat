46b16e8f62ce7e8fe19d3212ddee362e48952126
picarro-spulikkal <spulikkal@picarro.com>
2015-12-30 14:51:45 -0800

    Adding additional files with changes needed for replaying db3 file in CI

---
+++ selenium-wd/data/defn/replay-defn-curl-ci.bat
@@ -0,0 +1,1 @@
+curl -X POST --data-binary @%REPLAY_DEFN_FILE% http://localhost:8080/replay
--- selenium-wd/lib/InvokeReplayScript.cmd
+++ selenium-wd/lib/InvokeReplayScript.cmd
@@ -1,3 +1,3 @@
-cd "C:\Repositories\surveyor-qa\selenium-wd\data\defn"
-C:\Repositories\surveyor-qa\selenium-wd\data\defn\replay-defn-curl.bat replay-db3_651680dec9df496493148749d013121a.defn
+cd "%CI_WORKING_DIR%\selenium-wd\data\defn"
+%CI_WORKING_DIR%\selenium-wd\data\defn\replay-defn-curl-ci.bat replay-db3_%UNIQUE_IDENTIFIER%.defn
 exit
