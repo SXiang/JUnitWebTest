4a85f9f5be98af8eabb300ea3b3bae7b0b9992ce
picarro-spulikkal <spulikkal@picarro.com>
2015-12-30 12:14:36 -0800

    Script changes for running replay script in CI

--- selenium-wd/lib/InvokeReplayScript.cmd
+++ selenium-wd/lib/InvokeReplayScript.cmd
@@ -1,3 +1,3 @@
-cd "%CI_WORKING_DIR%\selenium-wd\data\defn" 
-%CI_WORKING_DIR%\selenium-wd\data\defn\replay-defn-curl.bat %UNIQUE_IDENTIFIER%_replay-db3.defn
+cd "C:\Repositories\surveyor-qa\selenium-wd\data\defn"
+C:\Repositories\surveyor-qa\selenium-wd\data\defn\replay-defn-curl.bat replay-db3_651680dec9df496493148749d013121a.defn
 exit
