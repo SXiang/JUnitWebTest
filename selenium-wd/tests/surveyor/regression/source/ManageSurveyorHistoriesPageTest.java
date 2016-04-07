429814a05798ddf1e3fe221fc4c457a29e306630
pvmahajan <pmahajan@picarro.com>
2016-04-07 10:16:13 -0500

    History note test cases

--- selenium-wd/tests/surveyor/regression/source/ManageSurveyorHistoriesPageTest.java
+++ selenium-wd/tests/surveyor/regression/source/ManageSurveyorHistoriesPageTest.java
@@ -3,6 +3,7 @@
  */
 package surveyor.regression.source;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static surveyor.scommon.source.SurveyorConstants.SQACUS;
 import static surveyor.scommon.source.SurveyorConstants.SQACUSLOC;
@@ -15,6 +16,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.support.PageFactory;
 
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import common.source.CryptoUtility;
+import common.source.Log;
+
+import surveyor.dataprovider.UserDataProvider;
 import surveyor.scommon.source.ManageSurveyorHistoriesPage;
 import surveyor.scommon.source.SurveyorBaseTest;
 import surveyor.scommon.source.SurveyorTestRunner;
@@ -81,5 +87,56 @@ public class ManageSurveyorHistoriesPageTest extends SurveyorBaseTest {
 		assertTrue("Picarro Support user not able to add new history note!",
 				manageSurveyorHistoriesPage.findExistingHistoryNote(SQACUS,
 						SQACUSLOC, SQACUSLOCSUR, strNote));
+	}
+	
+	/**
+	 * Test Case ID: TC134_TC1254_AddNoteMaxCharLimit
+	 * More than 1500 characters not allowed in Note
+	 * field present on Manage Add Surveyor History screen
+	 * 
+	 */
+	@Test
+	@UseDataProvider(value = UserDataProvider.USER_ADMIN_SUPPORT_PROVIDER, location = UserDataProvider.class)
+	public void TC134_TC1254_AddNoteMaxCharLimit(String user, String pwd) {
+		String str1467chars = "AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-AbcdefghI-Abcdefgh";
+		String str1468chars = str1467chars + "A";
+		String str1469chars = str1468chars + "A";
+		String strNote, strNote1500chars, strNote1501chars;
+		String surveyorUnit = SQACUS + " - " + SQACUSLOC + " - "
+				+ SQACUSLOCSUR;
+
+		String tcID;
+		if (user.equalsIgnoreCase("administrator")) {
+			tcID = "TC134";
+			strNote = tcID + " Automation Note "
+					+ testSetup.getFixedSizeRandomNumber(9);
+			strNote1500chars = strNote + str1468chars;
+			strNote1501chars = strNote + str1469chars;
+		} else {
+			tcID = "TC1254";
+			strNote = tcID + " Automation Note "
+					+ testSetup.getFixedSizeRandomNumber(9);
+			strNote1500chars = strNote + str1467chars;
+			strNote1501chars = strNote + str1468chars;
+		}
+		String password = CryptoUtility.decrypt(pwd);
+
+		Log.info("\nRunning - "
+				+ tcID
+				+ "_AddNoteMaxCharLimit - Test Description: More than 1500 characters not allowed in Note field present on Manage Add Surveyor History screen\n");
+
+		loginPage.open();
+		loginPage.loginNormalAs(user, password);
+
+		manageSurveyorHistoriesPage.open();
+		manageSurveyorHistoriesPage.addNewHistoryNote(surveyorUnit, strNote1500chars);
+		assertTrue("Administrator not able to add new history note!",
+				manageSurveyorHistoriesPage.findExistingHistoryNote(SQACUS,
+						SQACUSLOC, SQACUSLOCSUR, strNote1500chars));
+		
+		manageSurveyorHistoriesPage.addNewHistoryNote(surveyorUnit, strNote1501chars);
+		assertFalse("Administrator was able to add new history note!",
+				manageSurveyorHistoriesPage.findExistingHistoryNote(SQACUS,
+						SQACUSLOC, SQACUSLOCSUR, strNote1501chars));
 	}
 }
