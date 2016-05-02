1bd0f170271e3a193bd9a402e4921e5a939111cd
picarro-spulikkal <spulikkal@picarro.com>
2016-05-02 12:02:46 -0700

    add support for new report job type=ZIP

--- selenium-wd/tests/surveyor/dataprovider/PerformanceReportJobDataProvider.java
+++ selenium-wd/tests/surveyor/dataprovider/PerformanceReportJobDataProvider.java
@@ -55,10 +55,10 @@ public class PerformanceReportJobDataProvider extends ReportDataProvider {
 	public static Object[][] dataProviderReportJobPerformance() {
 		
 		return new Object[][] {				
-			{ "TC1841" /*TestCaseID*/, LIGHT_LOAD3_USER_ROW_ID  /*userDataRowID*/, 
-			   LIGHT_LOAD3_REPORT_DATA_ROW_ID /*reportDataRowID*/, 
-			   LIGHT_LOAD3_EXECUTIONS_FOR_BASELINES /*number of times to execute this test when generating baselines*/,
-			   ReportJobTestCategory.Light.toString()},
+//			{ "TC1841" /*TestCaseID*/, LIGHT_LOAD3_USER_ROW_ID  /*userDataRowID*/, 
+//			   LIGHT_LOAD3_REPORT_DATA_ROW_ID /*reportDataRowID*/, 
+//			   LIGHT_LOAD3_EXECUTIONS_FOR_BASELINES /*number of times to execute this test when generating baselines*/,
+//			   ReportJobTestCategory.Light.toString()},
 			{ "TC1842", MEDIUM_LOAD3_USER_ROW_ID, MEDIUM_LOAD3_REPORT_DATA_ROW_ID, MEDIUM_LOAD3_EXECUTIONS_FOR_BASELINES, ReportJobTestCategory.Medium.toString() },
 			{ "TC1843", HIGH_LOAD2_USER_ROW_ID, HIGH_LOAD2_REPORT_DATA_ROW_ID, HIGH_LOAD3_EXECUTIONS_FOR_BASELINES, ReportJobTestCategory.High.toString() },
 			{ "TC1844", ULTRA_HIGH_LOAD1_USER_ROW_ID, ULTRA_HIGH_LOAD1_REPORT_DATA_ROW_ID, ULTRA_HIGH_LOAD3_EXECUTIONS_FOR_BASELINES, ReportJobTestCategory.UltraHigh.toString() }
