ba55dc0b7ee7f809d6d0c2a0aad46f63cb034171
Steven Xiang <sxiang@picarro.com>
2016-05-04 19:38:20 -0700

    solved git diff issue on github

--- selenium-wd/tests/surveyor/scommon/source/ManageCustomersPage.java
+++ selenium-wd/tests/surveyor/scommon/source/ManageCustomersPage.java
@@ -6,16 +6,13 @@ package surveyor.scommon.source;
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.FindBy;
 
-import common.source.BaseHelper;
 import common.source.Log;
 import common.source.TestSetup;
 import surveyor.dataaccess.source.ResourceKeys;
