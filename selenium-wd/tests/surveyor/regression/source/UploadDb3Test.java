aa2c40f923584c263b032c6d570260cee05e21e2
picarro-spulikkal <spulikkal@picarro.com>
2016-05-12 15:12:13 -0700

    Simulator binary with mode change + test cleanup

--- selenium-wd/tests/surveyor/regression/source/UploadDb3Test.java
+++ selenium-wd/tests/surveyor/regression/source/UploadDb3Test.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 
 import common.source.Log;
 import common.source.TestContext;
-import common.source.TestSetup;
 import surveyor.scommon.source.SurveyorBaseTest;
 
 public class UploadDb3Test extends SurveyorBaseTest {
@@ -16,7 +15,6 @@ public class UploadDb3Test extends SurveyorBaseTest {
 	 * Unit Test to verify that surveys specified in test properties are correctly uploaded.
 	 * Pre-Req for running this test:
 	 * - In Test.properties:
-	 * 		surveyUpload.Enabled = false   <-- [This value is set to FALSE]
 	 * 		surveyUpload.Surveys = {6,1,46,60};{3,2,47,60}  <-- [Surveys to be uploaded are specified in this format: 
 	 * 					 										 {loginUserRowID1,db3AnalyzerRowID1,surveyRowID1,surveyRuntimeInSeconds1};{loginUserRowID2,db3AnalyzerRowID2,surveyRowID2,surveyRuntimeInSeconds2}]
 	 * @throws Exception
@@ -24,10 +22,6 @@ public class UploadDb3Test extends SurveyorBaseTest {
 	@Test
 	public void PushDB3Data_Test() throws Exception {
 		Log.info("\nRunning PushDB3Data_Test ...");
-		
-		TestSetup testSetupObj = TestContext.INSTANCE.getTestSetup();
-		if (!testSetupObj.isUploadSurveyEnabled()) {
-			testSetupObj.uploadSurveys();
-		}
+		TestContext.INSTANCE.getTestSetup().uploadSurveys();
 	}	
 }
