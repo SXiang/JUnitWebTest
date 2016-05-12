7b44b816cb09682c8e4e8569b060bc53c1fa3307
picarro-spulikkal <spulikkal@picarro.com>
2016-05-12 15:43:22 -0700

    add ability to trigger survey upload from CI

--- selenium-wd/lib/SetupSimulatorPreReqs-Local.cmd
+++ selenium-wd/lib/SetupSimulatorPreReqs-Local.cmd
@@ -1,1 +1,2 @@
 Powershell -command "& {Set-ExecutionPolicy Unrestricted -Force;Unblock-File -Path C:\Repositories\surveyor-qa\selenium-wd\lib\SetupSimulatorPreReqs.ps1;C:\Repositories\surveyor-qa\selenium-wd\lib\SetupSimulatorPreReqs.ps1 -isRunningLocally:$true -overwritePicarroAnalyzer:$true; exit;}"
+exit
