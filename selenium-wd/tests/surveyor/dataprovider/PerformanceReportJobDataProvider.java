4747130133b85aa28d827f04e5d5240ad2204e24
picarro-spulikkal <spulikkal@picarro.com>
2016-06-10 01:32:19 -0700

    report job perf tests with multiple views and large PDF size

--- selenium-wd/tests/surveyor/dataprovider/PerformanceReportJobDataProvider.java
+++ selenium-wd/tests/surveyor/dataprovider/PerformanceReportJobDataProvider.java
@@ -34,11 +34,27 @@ public class PerformanceReportJobDataProvider extends ReportDataProvider {
 	
 	private static final int HIGH_LOAD2_USER_ROW_ID = 10;
 	private static final int HIGH_LOAD2_REPORT_DATA_ROW_ID = 12;
-	private static final int HIGH_LOAD3_EXECUTIONS_FOR_BASELINES = 10;
+	private static final int HIGH_LOAD2_EXECUTIONS_FOR_BASELINES = 10;
 
 	private static final int ULTRA_HIGH_LOAD1_USER_ROW_ID = 10;
 	private static final int ULTRA_HIGH_LOAD1_REPORT_DATA_ROW_ID = 13;
-	private static final int ULTRA_HIGH_LOAD3_EXECUTIONS_FOR_BASELINES = 5;
+	private static final int ULTRA_HIGH_LOAD1_EXECUTIONS_FOR_BASELINES = 5;
+
+	private static final int HIGH_LOAD4_USER_ROW_ID = 10;
+	private static final int HIGH_LOAD4_REPORT_DATA_ROW_ID = 86;
+	private static final int HIGH_LOAD4_EXECUTIONS_FOR_BASELINES = 10;
+
+	private static final int ULTRA_HIGH_LOAD4_USER_ROW_ID = 10;
+	private static final int ULTRA_HIGH_LOAD4_REPORT_DATA_ROW_ID = 87;
+	private static final int ULTRA_HIGH_LOAD4_EXECUTIONS_FOR_BASELINES = 5;
+
+	private static final int HIGH_LOAD5_USER_ROW_ID = 10;
+	private static final int HIGH_LOAD5_REPORT_DATA_ROW_ID = 88;
+	private static final int HIGH_LOAD5_EXECUTIONS_FOR_BASELINES = 10;
+
+	private static final int ULTRA_HIGH_LOAD5_USER_ROW_ID = 10;
+	private static final int ULTRA_HIGH_LOAD5_REPORT_DATA_ROW_ID = 89;
+	private static final int ULTRA_HIGH_LOAD5_EXECUTIONS_FOR_BASELINES = 5;
 
 	public static final String REPORT_JOB_PERFORMANCE_PROVIDER = "dataProviderReportJobPerformance";
 
@@ -55,13 +71,17 @@ public class PerformanceReportJobDataProvider extends ReportDataProvider {
 	public static Object[][] dataProviderReportJobPerformance() {
 		
 		return new Object[][] {				
+			{ "TC1843-1", HIGH_LOAD4_USER_ROW_ID, HIGH_LOAD4_REPORT_DATA_ROW_ID, HIGH_LOAD4_EXECUTIONS_FOR_BASELINES, ReportJobTestCategory.High.toString() },
+			{ "TC1844-1", ULTRA_HIGH_LOAD4_USER_ROW_ID, ULTRA_HIGH_LOAD4_REPORT_DATA_ROW_ID, ULTRA_HIGH_LOAD4_EXECUTIONS_FOR_BASELINES, ReportJobTestCategory.UltraHigh.toString() },
+			{ "TC1843-2", HIGH_LOAD5_USER_ROW_ID, HIGH_LOAD5_REPORT_DATA_ROW_ID, HIGH_LOAD5_EXECUTIONS_FOR_BASELINES, ReportJobTestCategory.High.toString() },
+			{ "TC1844-2", ULTRA_HIGH_LOAD5_USER_ROW_ID, ULTRA_HIGH_LOAD5_REPORT_DATA_ROW_ID, ULTRA_HIGH_LOAD5_EXECUTIONS_FOR_BASELINES, ReportJobTestCategory.UltraHigh.toString() },
 			{ "TC1841" /*TestCaseID*/, LIGHT_LOAD3_USER_ROW_ID  /*userDataRowID*/, 
 			   LIGHT_LOAD3_REPORT_DATA_ROW_ID /*reportDataRowID*/, 
 			   LIGHT_LOAD3_EXECUTIONS_FOR_BASELINES /*number of times to execute this test when generating baselines*/,
 			   ReportJobTestCategory.Light.toString()},
 			{ "TC1842", MEDIUM_LOAD3_USER_ROW_ID, MEDIUM_LOAD3_REPORT_DATA_ROW_ID, MEDIUM_LOAD3_EXECUTIONS_FOR_BASELINES, ReportJobTestCategory.Medium.toString() },
-			{ "TC1843", HIGH_LOAD2_USER_ROW_ID, HIGH_LOAD2_REPORT_DATA_ROW_ID, HIGH_LOAD3_EXECUTIONS_FOR_BASELINES, ReportJobTestCategory.High.toString() },
-			{ "TC1844", ULTRA_HIGH_LOAD1_USER_ROW_ID, ULTRA_HIGH_LOAD1_REPORT_DATA_ROW_ID, ULTRA_HIGH_LOAD3_EXECUTIONS_FOR_BASELINES, ReportJobTestCategory.UltraHigh.toString() }
+			{ "TC1843", HIGH_LOAD2_USER_ROW_ID, HIGH_LOAD2_REPORT_DATA_ROW_ID, HIGH_LOAD2_EXECUTIONS_FOR_BASELINES, ReportJobTestCategory.High.toString() },
+			{ "TC1844", ULTRA_HIGH_LOAD1_USER_ROW_ID, ULTRA_HIGH_LOAD1_REPORT_DATA_ROW_ID, ULTRA_HIGH_LOAD1_EXECUTIONS_FOR_BASELINES, ReportJobTestCategory.UltraHigh.toString() }
 		};
 	}
 }
