f367a154f737a69c9b5a8a9c0b737785e4278f98
picarro-chathuri <cnanayakkara@picarro.com>
2016-08-11 10:10:16 -0700

    un comment after class

--- selenium-wd/tests/surveyor/scommon/source/SurveyorBaseTest.java
+++ selenium-wd/tests/surveyor/scommon/source/SurveyorBaseTest.java
@@ -180,7 +180,7 @@ public class SurveyorBaseTest {
 	/**
 	 * @throws java.lang.Exception
 	 */
-	//@AfterClass
+	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 		logoutQuitDriver();		
 		
