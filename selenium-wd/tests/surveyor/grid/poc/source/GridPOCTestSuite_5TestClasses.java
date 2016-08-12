74eb963bdc545bc29a41b27358b753f6117490ce
picarro-spulikkal <spulikkal@picarro.com>
2016-08-12 15:05:45 -0700

    5 test batch class

---
+++ selenium-wd/tests/surveyor/grid/poc/source/GridPOCTestSuite_5TestClasses.java
@@ -0,0 +1,29 @@
+package surveyor.grid.poc.source;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+import common.source.Log;
+import surveyor.scommon.source.SurveyorParallelSuite;
+
+@RunWith(SurveyorParallelSuite.class)
+@SuiteClasses({ 
+	GridPOCTestClass1.class
+	,GridPOCTestClass2.class
+	,GridPOCTestClass3.class
+	,GridPOCTestClass4.class
+	,GridPOCTestClass5.class})
+public class GridPOCTestSuite_5TestClasses {
+	
+	@BeforeClass 
+    public static void setUpClass() {      
+        Log.info("Setup for GridPOCTestSuite_5Tests executing...");
+    }
+
+    @AfterClass 
+    public static void tearDownClass() { 
+    	Log.info("Teardown for GridPOCTestSuite_5Tests executing...");
+    }
+}
