b933079d69975a2ada071786a7cd0ba78527c48d
picarro-spulikkal <spulikkal@picarro.com>
2016-08-15 17:05:15 -0700

    US3279 - Push data for Ethane related surveys used in tests

--- selenium-wd/tests/surveyor/unittest/source/DbSeedExecutorTest.java
+++ selenium-wd/tests/surveyor/unittest/source/DbSeedExecutorTest.java
@@ -144,7 +144,8 @@ public class DbSeedExecutorTest {
 	
 	private void verifySurveySeedDataIsPresent() throws IOException, FileNotFoundException, SQLException {
 		// Verify seed data pushed correctly for each survey tag.
-		final String[] surveyTags = {"assessment-1", "assessment-2", "Ethane1MinSurvey", "iso-cap-1", "iso-cap-2", "man-pic-1","man-pic-2","op-pic","op-sqacudr","rr-pic","rr-sqacudr-1","rr-sqacudr-2","stnd-pic",
+		final String[] surveyTags = {"assessment-1", "assessment-2", "EthaneStnd3","EthaneStnd2","EthaneStnd","EthaneRR","EthaneOpertor2","EthaneOpertor1","Ethane1MinSurvey", 
+				"iso-cap-1", "iso-cap-2", "man-pic-1","man-pic-2","op-pic","op-sqacudr","rr-pic","rr-sqacudr-1","rr-sqacudr-2","stnd-pic",
 				"standard_test-1", "standard_test-2", "standard_test-3", "stnd-sqacudr","stnd-sqacudr-1","stnd-sqacudr-2","stnd-sqacudr-3"};
 		
 		boolean isRedate = false;
