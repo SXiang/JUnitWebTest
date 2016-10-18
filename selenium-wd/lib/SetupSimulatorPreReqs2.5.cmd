24fb98c095b1fa027c57c9dea6e46b994988b554
Steven Xiang <sxiang@picarro.com>
2016-10-18 15:57:36 -0700

    Rollback SetupSimulatorProReqs.ps1 to 2.4 virsion

--- selenium-wd/lib/SetupSimulatorPreReqs2.5.cmd
+++
@@ -1,2 +0,0 @@
-Powershell -command "& {Set-ExecutionPolicy Unrestricted -Force;Unblock-File -Path %CI_WORKING_DIR%\selenium-wd\lib\SetupSimulatorPreReqs.ps1;%CI_WORKING_DIR%\lib\SetupSimulatorPreReqs.ps1 -isRunningLocally:$false; exit;}"
-exit
