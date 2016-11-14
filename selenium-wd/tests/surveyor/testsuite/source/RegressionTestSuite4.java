9eb93a6e75b32c02e3bd43d4a1dde3d701081aab
picarro-spulikkal <spulikkal@picarro.com>
2016-11-14 05:02:43 -0800

    spread survey view tests in parallel test suite

--- selenium-wd/tests/surveyor/testsuite/source/RegressionTestSuite4.java
+++ selenium-wd/tests/surveyor/testsuite/source/RegressionTestSuite4.java
@@ -6,17 +6,17 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 
 import common.source.Log;
+import surveyor.regression.source.ACLandVisibilityTest;
 import surveyor.regression.source.LicensedFeatureAndVisibilityTest;
 import surveyor.regression.source.MeasurementSessionsPageTest;
-import surveyor.regression.source.SurveyViewPageTest;
 import surveyor.regression.source.SurveyViewPageTest1;
 import surveyor.regression.source.SurveyViewPageTest2;
 import surveyor.scommon.source.SurveyorParallelSuite;
 
 @RunWith(SurveyorParallelSuite.class)
 @SuiteClasses({
-	MeasurementSessionsPageTest.class
-	,SurveyViewPageTest.class
+	ACLandVisibilityTest.class
+	,MeasurementSessionsPageTest.class
 	,SurveyViewPageTest1.class
 	,SurveyViewPageTest2.class
 	,LicensedFeatureAndVisibilityTest.class
