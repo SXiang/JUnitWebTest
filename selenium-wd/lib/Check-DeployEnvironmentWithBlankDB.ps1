753f297384e0cc681998cb1a30eebf9a6fab909b
picarro-spulikkal <spulikkal@picarro.com>
2016-11-17 02:42:56 -0800

    check deployment environment script changes

--- selenium-wd/lib/Check-DeployEnvironmentWithBlankDB.ps1
+++ selenium-wd/lib/Check-DeployEnvironmentWithBlankDB.ps1
@@ -3,9 +3,10 @@
 #   .\Check-DeployEnvironmentWithBlankDB.ps1 `
 #        -BuildWorkingDir "C:\Repositories\surveyor-qa" `
 #        -AutomationReportingAPIBaseUrl "http://localhost:63087" `
-#        -DeployIntervalInDays 1 `
-#        -EnvironmentTeamCityBuildConfigId "<build_config_id>" `
-#        -EnvironmentId 1
+#        -DeployIntervalInDays 7 `
+#        -EnvironmentTeamCityBuildConfigId "Surveyor_2_SqaautoDevelop" `
+#        -EnvironmentId 1  `
+#        -WaitTimeInMinutesForDeployment 20
 
 param
 (
@@ -22,7 +23,10 @@ param
   [String] $EnvironmentTeamCityBuildConfigId,      # Provide environmentTeamCityBuildConfigId of the environment along with EnvironmentId to ensure incorrect environment does not get deployed.
 
   [Parameter(Mandatory=$true)]
-  [int] $EnvironmentId
+  [int] $EnvironmentId,
+
+  [Parameter(Mandatory=$true)]
+  [int] $WaitTimeInMinutesForDeployment
 ) 
 
 $libFolder = "selenium-wd\lib"
@@ -54,10 +58,25 @@ $Body = @{
     EnvironmentId = $EnvironmentId
     TeamCityBuildConfigId = $EnvironmentTeamCityBuildConfigId
     DeployIntervalInDays = $DeployIntervalInDays
+    DeployWithBlankDB = "true"
 }
 
 $jsonBody = (ConvertTo-Json $Body)
 Write-Host "Triggering $EnvironmentTeamCityBuildConfigId deployment - $AutomationReportingAPIBaseUrl/$triggerDeploymentApiUrl ..." 
 $response = Invoke-WebRequest -Uri "$AutomationReportingAPIBaseUrl/$triggerDeploymentApiUrl" -Headers $Headers -Method POST -Body $jsonBody -ContentType $postContentType
 $json = $response.Content | ConvertFrom-Json
-Write-Host "Triggering $EnvironmentTeamCityBuildConfigId deployment successful! Response -> $json"
+Write-Host "Triggering $EnvironmentTeamCityBuildConfigId deployment successful!"
+
+# ----------------------------------------------------------------------------------------------
+# Wait for deployment to complete.
+# ----------------------------------------------------------------------------------------------
+
+$iteration = 1
+while ($WaitTimeInMinutesForDeployment -gt 0) {
+    Write-Host "[Iteration-$iteration] Waiting for deployment to complete... Remaining wait time = $WaitTimeInMinutesForDeployment minutes ..."
+    Start-Sleep -Seconds 60
+    $WaitTimeInMinutesForDeployment--
+    $iteration++
+}
+
+Write-Host "Done with script execution!"
