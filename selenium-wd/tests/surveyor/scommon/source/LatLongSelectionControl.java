de3e16103d0b40e768f79914f4f9faab558dfd00
picarro-spulikkal <spulikkal@picarro.com>
2016-12-01 16:07:14 -0800

    add additional wait in setCustomerBoundaryName method

--- selenium-wd/tests/surveyor/scommon/source/LatLongSelectionControl.java
+++ selenium-wd/tests/surveyor/scommon/source/LatLongSelectionControl.java
@@ -223,6 +223,8 @@ public class LatLongSelectionControl extends BaseControl {
 	public LatLongSelectionControl setCustomerBoundaryName(String name) {
 		Log.info("Wait for boundary name text field to be clickable");
 		WebElementExtender.waitForElementToBeClickable(timeout, driver, selectByNameTextField);
+		Log.info("Wait for boundary name text field to be enabled");
+		this.waitForElementToBeEnabled(selectByNameTextField);
 		Log.info("Set customer boundary name '"+name+"'");
 		selectByNameTextField.sendKeys(name);
 		this.waitForAutoCompleteListToOpen();
