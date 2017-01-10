d08af5afd4976fdbadae7cfa7264f97f2d672e20
picarro-spulikkal <spulikkal@picarro.com>
2017-01-10 15:44:00 -0800

    added checks before installing certs for test analyzers. adding timeout while waiting on upgrade to complete

--- selenium-wd/lib/HelperScripts/CertificateHelpers.ps1
+++ selenium-wd/lib/HelperScripts/CertificateHelpers.ps1
@@ -6,6 +6,30 @@
 ----------------------------------------------------------------------------------------------------------------------------------#>
 
 # -----------------------------------------------------------------------------------------------------------------------
+# Imports certificate is not already installed, to specified store and location with specified storage flags.
+# -----------------------------------------------------------------------------------------------------------------------
+function Import-CertificateWithCheck($certificatePath,
+    [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags] $keyStorageFlags,   # specifies storageFlags to use (eg. Current User/LocalMachine)
+    [System.Security.Cryptography.X509Certificates.StoreName] $storeName,                   # specifies name of the store to import to.
+    [System.Security.Cryptography.X509Certificates.StoreLocation] $storeLocation            # specifies location where to import the cert.
+) {
+    "Reading subject name from certificate ..."
+    $subjectNm = Get-SubjectName -certificatePath $certificatePath -keyStorageFlags $keyStorageFlags
+    $thumbprint = Get-Thumbprint -certificatePath $certificatePath -keyStorageFlags $keyStorageFlags
+    "Subject name=[$subjectNm]; Thumbprint=[$thumbprint]"
+    "Checking if certificate with thumbprint=[$thumbprint] is already installed ..."
+    $findType = [System.Security.Cryptography.X509Certificates.X509FindType]::FindByThumbprint
+    $certFound = Find-Certificate -findIdentifier "$thumbprint" -storeName $storeName -storeLocation $storeLocation -findType $findType
+    "Certificate CHECK STATUS. Found='$certFound'"
+    if ($certFound -eq $false) {
+        "Importing certificate ..."
+        Import-Certificate -certificatePath "$certificatePath" -keyStorageFlags $keyStorageFlags -storeName $storeName -storeLocation $storeLocation
+        "Certificate imported."
+    }
+}
+
+
+# -----------------------------------------------------------------------------------------------------------------------
 # Imports specified certificate to specified store and location with the specified storage flags.
 # 
 # SAMPLE USAGE: 
@@ -22,7 +46,75 @@ function Import-Certificate($certificatePath,
     $x509 = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
     $x509.Import($certificatePath, "", $keyStorageFlags)
     $store = New-Object System.Security.Cryptography.X509Certificates.X509Store($storeName, $storeLocation)
+    "Opening certificate store for importing ..."
     $store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadWrite)
+    "Adding certificate to store ..."
     $store.Add($x509);
     $store.Close();
+}
+
+
+# -----------------------------------------------------------------------------------------------------------------------
+# Returns subject name from the certificate.
+# -----------------------------------------------------------------------------------------------------------------------
+function Get-SubjectName($certificatePath,
+    [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags] $keyStorageFlags
+) {
+    $x509 = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
+    $x509.Import($certificatePath, "", $keyStorageFlags)
+    $x509.Subject
+}
+
+
+# -----------------------------------------------------------------------------------------------------------------------
+# Returns thumbprint from the certificate.
+# -----------------------------------------------------------------------------------------------------------------------
+function Get-Thumbprint($certificatePath,
+    [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags] $keyStorageFlags
+) {
+    $x509 = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
+    $x509.Import($certificatePath, "", $keyStorageFlags)
+    $x509.Thumbprint
+}
+
+
+# -----------------------------------------------------------------------------------------------------------------------
+# Returns whether a certificate was found in specified cert store and location.
+# 
+# SAMPLE USAGE: 
+#   $subjectName = "simauto-analyzer1"
+#   $domainNameSuffix = ".surveyordev.com"
+#   $storeNameEnum = [System.Security.Cryptography.X509Certificates.StoreName]::My
+#   $storeLocationEnum = [System.Security.Cryptography.X509Certificates.StoreLocation]::LocalMachine
+#   $foundCert = Find-Certificate -subjectName "$subjectName" -domainNameSuffix $domainNameSuffix -storeName $storeNameEnum -storeLocation $storeLocationEnum
+#   "Got certificate = '$foundCert'"
+# -----------------------------------------------------------------------------------------------------------------------
+function Find-Certificate($subjectName, $domainNameSuffix,
+    [System.Security.Cryptography.X509Certificates.StoreName] $storeName,                   # specifies name of the store to import to.
+    [System.Security.Cryptography.X509Certificates.StoreLocation] $storeLocation            # specifies location where to import the cert.    
+) {
+    $retVal = $false
+    $findType = [System.Security.Cryptography.X509Certificates.X509FindType]::FindBySubjectName
+    $findIdentifier = $subjectName + $domainNameSuffix
+    $retVal = Find-Certificate -findIdentifier "$findIdentifier" -storeName $storeNameEnum -storeLocation $storeLocationEnum -findType $findType
+    $retVal
+}
+
+
+# -----------------------------------------------------------------------------------------------------------------------
+# Returns whether a certificate was found in specified cert store and location using specified FindType.
+# -----------------------------------------------------------------------------------------------------------------------
+function Find-Certificate($findIdentifier,
+    [System.Security.Cryptography.X509Certificates.StoreName] $storeName,                   # specifies name of the store to import to.
+    [System.Security.Cryptography.X509Certificates.StoreLocation] $storeLocation,            # specifies location where to import the cert.    
+    [System.Security.Cryptography.X509Certificates.X509FindType] $findType
+) {
+    $retVal = $false
+    $store = New-Object System.Security.Cryptography.X509Certificates.X509Store($storeName, $storeLocation)
+    $store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadOnly)
+    $coll = $store.Certificates.Find($findType, $findIdentifier, $false)
+    if ($coll.Count -gt 0) {
+        $retVal = $true
+    }
+    $retVal
 }
---
+++ selenium-wd/lib/HelperScripts/Test-CertificateHelpers.ps1
@@ -0,0 +1,48 @@
+﻿. C:\Repositories\surveyor-qa\selenium-wd\lib\HelperScripts\CertificateHelpers.ps1
+
+### Method Tests: 
+$executeTests = $true
+$testsToRun = "Test-01","Test-02"
+
+if ($executeTests) {
+    if ($testsToRun.Contains("Test-01")) {
+        #### [Test-01]: PRINT SUBJECT NAME & THUMBPRINT FROM ALL CERTS IN A FOLDER.
+        $certFolder = "C:\Repositories\surveyor-qa\selenium-wd\data\certs"
+        $FILTERS = "*.pfx","*.pem"
+        $FILTERS | % {
+            $filter = $_
+            Get-ChildItem $certFolder -Filter "$filter" -Recurse | % {
+                $file = $_
+                $filename = $file.Name
+                $certFilePath = $file.FullName
+
+                $storageFlagsEnum = [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::MachineKeySet
+                $subjectNm = Get-SubjectName -certificatePath "$certFilePath" -keyStorageFlags $storageFlagsEnum
+                if ($subjectNm -ne $NULL -and $subjectNm -ne "") {
+                    $subjectNm = $subjectNm.Replace("CN=", "")
+                }
+                $thumbprint = Get-Thumbprint -certificatePath "$certFilePath" -keyStorageFlags $storageFlagsEnum
+                "Cert=[$filename]; SubjectName=[$subjectNm]; Thumbprint=[$thumbprint]"
+            }
+        }
+    }
+
+    if ($testsToRun.Contains("Test-02")) {
+        #### [Test-02]: CHECK AND IMPORT CERTIFICATE.
+        $certFolder = "C:\Repositories\surveyor-qa\selenium-wd\data\certs"
+        $storageFlagsEnum = [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::MachineKeySet
+        $storeNameEnum = [System.Security.Cryptography.X509Certificates.StoreName]::My
+        $storeLocationEnum = [System.Security.Cryptography.X509Certificates.StoreLocation]::LocalMachine
+        $FILTERS = "*.pfx"
+        $FILTERS | % {
+            $filter = $_
+            Get-ChildItem $certFolder -Filter "$filter" -Recurse | % {
+                $file = $_
+                $filename = $file.Name
+                $certFilePath = $file.FullName
+
+                Import-CertificateWithCheck -certificatePath $certFilePath -keyStorageFlags $storageFlagsEnum -storeName $storeNameEnum -storeLocation $storeLocationEnum
+            }
+        }
+    }
+}
