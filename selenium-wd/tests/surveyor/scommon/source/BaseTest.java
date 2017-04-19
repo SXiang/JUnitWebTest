b81d8f39a7ffb0ab349fa69d4eb54ab3b0e7e96e
Steven Xiang <sxiang@picarro.com>
2017-04-19 10:21:32 -0700

    Added wait after long run of DB3

--- selenium-wd/tests/surveyor/scommon/source/BaseTest.java
+++ selenium-wd/tests/surveyor/scommon/source/BaseTest.java
@@ -538,6 +538,7 @@ public class BaseTest {
 			driverViewPageAction.clickOnShutdownConfirmButton("", -1);
 			testEnvironmentAction.idleForSeconds(String.valueOf(10), -1);
 			TestContext.INSTANCE.getTestSetup().checkPostSurveySessionFromDB3(analyzerName, analyzerSharedKey, surveyorName);
+			testEnvironmentAction.idleForSeconds(String.valueOf(surveyRuntimeInSeconds/10), -1);
 		}
 
 		TestSetup.stopAnalyzer();
