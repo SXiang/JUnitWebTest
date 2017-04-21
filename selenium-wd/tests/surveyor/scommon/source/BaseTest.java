55e77da19199cbf7e44821809d51aa5638cb837f
Steven Xiang <sxiang@picarro.com>
2017-04-21 15:45:05 -0700

    Fix statcoverflow error in addTestSurvey

--- selenium-wd/tests/surveyor/scommon/source/BaseTest.java
+++ selenium-wd/tests/surveyor/scommon/source/BaseTest.java
@@ -458,7 +458,7 @@ public class BaseTest {
 		return addTestSurvey(analyzerName, analyzerSharedKey, analyzerType, getTestSetup().getLoginUser(), getTestSetup().getLoginPwd());
 	}
 	public Map<String, String> addTestSurvey(String analyzerName, String analyzerSharedKey, String userName, String password, SurveyType... surveyTypes) throws Exception{
-		return addTestSurvey(analyzerName, analyzerSharedKey, userName, password, surveyTypes);
+		return addTestSurvey(analyzerName, analyzerSharedKey, AnalyzerType.METHANE, userName, password, surveyTypes);
 	}
 	public Map<String, String> addTestSurvey(String analyzerName, String analyzerSharedKey, AnalyzerType analyzerType, String userName, String password, SurveyType... surveyTypes) throws Exception{
 		int surveyRuntimeInSeconds = 2;
