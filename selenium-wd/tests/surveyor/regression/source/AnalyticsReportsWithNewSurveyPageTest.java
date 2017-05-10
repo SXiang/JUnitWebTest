516940a8b5f79e82564d194168548555ea8bcd7a
Shirish Pulikkal <spulikkal@picarro.com>
2017-05-10 12:22:00 -0700

    added new Analytics survey/report test case - TC2418

--- selenium-wd/tests/surveyor/regression/source/AnalyticsReportsWithNewSurveyPageTest.java
+++ selenium-wd/tests/surveyor/regression/source/AnalyticsReportsWithNewSurveyPageTest.java
@@ -1,6 +1,8 @@
 package surveyor.regression.source;
 
+import common.source.FunctionUtil;
 import common.source.Log;
+import common.source.TestContext;
 
 import static org.junit.Assert.*;
 import static surveyor.scommon.source.SurveyorConstants.PICADMINPSWD;
@@ -12,20 +14,30 @@ import java.util.Map;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.support.PageFactory;
 
 import com.tngtech.java.junit.dataprovider.UseDataProvider;
 
 import org.junit.Test;
+import org.junit.rules.TestName;
+
 import surveyor.scommon.actions.LoginPageActions;
+import surveyor.scommon.actions.ManageCustomerPageActions;
 import surveyor.scommon.actions.ManageLocationPageActions;
+import surveyor.scommon.actions.ManageUsersPageActions;
+import surveyor.scommon.entities.CustomerSurveyInfoEntity;
 import surveyor.scommon.entities.BaseReportEntity.SurveyModeFilter;
+import surveyor.scommon.generators.TestDataGenerator;
 import surveyor.dataaccess.source.Analyzer.CapabilityType;
+import surveyor.dataaccess.source.Customer;
 import surveyor.dataprovider.AnalyticReportDataProvider;
+import surveyor.dbseed.source.DbSeedExecutor;
 import surveyor.scommon.actions.ComplianceReportsPageActions;
 import surveyor.scommon.source.SurveyorTestRunner;
 import surveyor.scommon.source.BaseReportsPageActionTest;
+import surveyor.scommon.source.BaseTest;
 import surveyor.scommon.source.ComplianceReportsPage;
 import surveyor.scommon.source.DriverViewPage;
 import surveyor.scommon.source.HomePage;
@@ -40,8 +52,14 @@ public class AnalyticsReportsWithNewSurveyPageTest extends BaseReportsPageAction
 
 	private static ComplianceReportsPageActions complianceReportsPageAction;
 	private static ManageLocationPageActions manageLocationPageActions;
+	private static LoginPageActions loginPageAction;
+
 	private static DriverViewPage driverViewPage;
 	private static Map<String, String> testAccount, testSurvey;
+
+	@Rule
+	public TestName testName = new TestName();
+
 	@BeforeClass
 	public static void beforeClass() {
 		initializeTestObjects();
@@ -62,11 +80,13 @@ public class AnalyticsReportsWithNewSurveyPageTest extends BaseReportsPageAction
 		// Select run mode here.
 		setPropertiesForTestRunMode();
 
-		if(testAccount == null){
-			testAccount = createTestAccount("Analytics_Report", CapabilityType.Ethane);
-			testSurvey = addTestSurvey(testAccount.get("analyzerName"), testAccount.get("analyzerSharedKey"), CapabilityType.Ethane
-					,testAccount.get("userName"), testAccount.get("userPassword"), 1500, SurveyType.Analytics);
-			pushGisData(testAccount.get("customerId"));
+		if (!testName.getMethodName().startsWith("TC2418_AnalyticsLowSurveyMinAmplitudeAndHighRankingMinAmplitude")) {
+			if(testAccount == null){
+				testAccount = createTestAccount("Analytics_Report", CapabilityType.Ethane);
+				testSurvey = addTestSurvey(testAccount.get("analyzerName"), testAccount.get("analyzerSharedKey"), CapabilityType.Ethane
+						,testAccount.get("userName"), testAccount.get("userPassword"), 1500, SurveyType.Analytics);
+				pushGisData(testAccount.get("customerId"));
+			}
 		}
 	}
 
@@ -98,7 +118,7 @@ public class AnalyticsReportsWithNewSurveyPageTest extends BaseReportsPageAction
 	 * @throws Exception
 	 */
 	protected static void initializePageActions() throws Exception {
-		new LoginPageActions(getDriver(), getBaseURL(), getTestSetup());
+		loginPageAction = new LoginPageActions(getDriver(), getBaseURL(), getTestSetup());
 		complianceReportsPageAction = new ComplianceReportsPageActions(getDriver(), getBaseURL(), getTestSetup());
 		manageLocationPageActions = new ManageLocationPageActions(getDriver(), getBaseURL(), getTestSetup());
 		setReportsPage((ComplianceReportsPage)complianceReportsPageAction.getPageObject());
@@ -148,6 +168,91 @@ public class AnalyticsReportsWithNewSurveyPageTest extends BaseReportsPageAction
 			String downloadPath = complianceReportsPageAction.getComplianceReportsPage().getDownloadPath(ReportFileType.PDF, reportTitle);
 			assertTrue(complianceReportsPageAction.getComplianceReportsPage().verifyIndicationTable(downloadPath, reportTitle, true));
 			getHomePage().logout();
+		}
+	}
+
+	/**
+	 *  Test Case ID: TC2418_AnalyticsLowSurveyMinAmplitudeAndHighRankingMinAmplitude() {
+	 *  Description: Analytics - Low Survey Min Amplitude and High Ranking Min Amplitude
+	 *	Script:
+	 *	- Log into the UI as Picarro Admin
+	 *	- Navigate to the Manage Locations page
+	 *	- Select a customer Location and click Edit
+	 *	- Scroll down to the Survey Mode: Analytics section
+	 *	- Set the Survey Min Amplitude to a low value like 0.035
+	 *	- Set the Ranking Min Amplitude to a high value like 2.5 and click OK
+	 *	- Log into the tablet as a user of that customer
+	 *	- Enter a tag, select the appropriate environmental conditions and select Survey Mode: Analytics and click Start Survey
+	 *	- Collect peaks of different amplitudes, from below 1.0 to above 3
+	 *	- After the survey has been uploaded, log into the UI and navigate to the Compliance Reports page
+	 *	- Click on New Compliance Report, enter a Report Title, select Report Mode: Analytics, select a boundary that includes the above survey area, select LISAs and indications from the View options and select Indication Table and click OK
+	 *	- Once the report generates, check the SSRS PDF
+	 *
+	 *	Verifications:
+	 *	- All of the indications below the Ranking Min Amplitude should have been filtered out.
+	 * @throws Exception
+	**/
+	@Test
+	@UseDataProvider(value = AnalyticReportDataProvider.ANALYTIC_REPORT_DATA_PROVIDER_TC2418, location = AnalyticReportDataProvider.class)
+	public void TC2418_AnalyticsLowSurveyMinAmplitudeAndHighRankingMinAmplitude(
+			String testCaseID, Integer userDataRowID, Integer reportDataRowID1, Integer reportDataRowID2) throws Exception {
+		Log.info("\nRunning TC2418_AnalyticsLowSurveyMinAmplitudeAndHighRankingMinAmplitude ...");
+
+		// Create location with desired min amp. Generate survey with multiple peaks above and below Ranking min amp.
+
+		final int DB3_ANALYZER_ROW_ID = 68;	 	  /* TestEnvironment datasheet rowID (specifies Analyzer, Replay DB3) */
+		final int SURVEY_ROW_ID = 61;	 		  /* Survey information  */
+		final int SURVEY_RUNTIME_IN_SECONDS = 200; /* Number of seconds to run the survey for. */
+		final int newCustomerRowID = 14;
+		final int newLocationRowID = 18;
+		final int newCustomerUserRowID = 28;
+		final int newSurveyorRowID = 26;
+		final int newAnalyzerRowID = 24;
+		final int newRefGasBottleRowID = 7;
+
+		loginPageAction.open(EMPTY, NOTSET);
+		loginPageAction.login(EMPTY, getUserRowID(userDataRowID));   /* Picarro Admin */
+
+		CustomerSurveyInfoEntity custSrvInfo = new CustomerSurveyInfoEntity(newCustomerRowID, newLocationRowID, newCustomerUserRowID, newAnalyzerRowID,
+				newSurveyorRowID, newRefGasBottleRowID, DB3_ANALYZER_ROW_ID, SURVEY_RUNTIME_IN_SECONDS, SURVEY_ROW_ID);
+		custSrvInfo.setPushGISSeedData(true);
+		custSrvInfo.setRetainGISSeedData(true);
+
+		try {
+			new TestDataGenerator().generateNewCustomerAndSurvey(custSrvInfo, (driverPageAction) -> {
+				assertTrue(driverPageAction.verifyCorrectAnalyticsSurveyActiveMessageIsShownOnMap(EMPTY, NOTSET));
+				return true;
+			});
+
+			loginPageAction.open(EMPTY, NOTSET);
+			loginPageAction.getLoginPage().loginNormalAs(ManageUsersPageActions.workingDataRow.get().username, ManageUsersPageActions.workingDataRow.get().password);
+
+			complianceReportsPageAction.open(EMPTY, getReportRowID(reportDataRowID1));
+			createNewReport(complianceReportsPageAction, getReportRowID(reportDataRowID1));
+			waitForReportGenerationToComplete(complianceReportsPageAction, getReportRowID(reportDataRowID1));
+			complianceReportsPageAction.openComplianceViewerDialog(EMPTY, getReportRowID(reportDataRowID1));
+			complianceReportsPageAction.clickOnComplianceViewerPDF(EMPTY, getReportRowID(reportDataRowID1));
+			complianceReportsPageAction.waitForPDFDownloadToComplete(EMPTY, getReportRowID(reportDataRowID1));
+
+	        assertTrue(complianceReportsPageAction.verifyLISAsIndicationTableInfo(EMPTY, getReportRowID(reportDataRowID1)));
+		} catch (Exception ex) {
+			BaseTest.reportTestFailed(ex, AnalyticsReportsWithNewSurveyPageTest.class.getName());
+		} finally {
+			cleanupReports(ComplianceReportsPageActions.workingDataRow.get().title, TestContext.INSTANCE.getLoggedInUser());
+			// Remove GIS seed from the customer.
+			FunctionUtil.warnOnError(() -> DbSeedExecutor.cleanUpGisSeed(Customer.getCustomer(ManageCustomerPageActions.workingDataRow.get().name).getId()));
+		}
+	}
+
+	private void cleanupReports(String rptTitle, String strCreatedBy) throws Exception {
+		// Delete report before deleting GIS data pushed by test to prevent FK constraint violation.
+		// Delete both the original report and the copy compliance report.
+		for (int i = 0; i < 2; i++) {
+			loginPageAction.open(EMPTY, NOTSET);
+			loginPageAction.login(String.format("%s:%s", PICDFADMIN, PICADMINPSWD), NOTSET);
+
+			complianceReportsPageAction.open(EMPTY, NOTSET);
+			complianceReportsPageAction.getComplianceReportsPage().searchAndDeleteReport(rptTitle, strCreatedBy);
 		}
 	}
 }
