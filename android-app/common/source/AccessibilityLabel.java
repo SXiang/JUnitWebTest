7c96fe2f126ac3cdbd3f4271f851dd02a62a09ee
Shirish Pulikkal <spulikkal@picarro.com>
2017-06-23 11:38:49 -0700

    add new file

---
+++ android-app/common/source/AccessibilityLabel.java
@@ -0,0 +1,19 @@
+package common.source;
+
+// TODO: Localize these.
+public class AccessibilityLabel {
+
+	public static class MapScreen {
+		public static final String TOGGLE_MODE_BTN = "Toggle Analyzer mode";
+		public static final String RESET_MAX_BTN = "Reset max concentration value";
+		public static final String INVESTIGATE_BTN = "Investigate PCubed Leaks or Gaps";
+	}
+
+	public static class LoginDialog {
+		public static final String SERVER_URL_EDIT_VIEW = "Enter server name";
+		public static final String USERNAME_EDIT_VIEW = "Enter username";
+		public static final String PASSWORD_EDIT_VIEW = "Enter password";
+		public static final String SUBMIT_BTN = "Submit button";
+		public static final String CANCEL_BTN = "Cancel Button";
+	}
+}
