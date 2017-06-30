cac9c6be1ac17b66c529c0a94b2e884a9a389a12
Emma Kresch <ekresch@picarro.com>
2017-06-30 10:50:12 -0700

    fixed api payload class for getLeakListByBoxId

--- selenium-wd/tests/surveyor/api/entities/Payload.java
+++ selenium-wd/tests/surveyor/api/entities/Payload.java
@@ -1,23 +1,49 @@
-package surveyor.apitest.source;
+package surveyor.api.entities;
 
 public class Payload {
 
-	//final String reportId = "53481c9f-e2de-0a16-b742-39dff622cf04";
-	//final String type = "Indication";
-	//String body = {"Top":37.396346133189255,"Bottom":37.39198194353354,"Left":-121.9866943359375,"Right":-121.981201171875};
-	final double Top = 37.396346133189255;
-	final double Bottom = 37.39198194353354;
-	final double Left = 37.39198194353354;
-	final double Right = -121.981201171875;
-	//final = ["Top":37.396346133189255?"Bottom":37.39198194353354?"Left":-121.9866943359375?"Right":-121.981201171875;]
-	}			
-//	Payload (String reportId, String type, double Top, double Bottom, double Left, double Right){
-//		this.reportId 
-//		this.type
-//		this.Top
-//		this.Bottom
-//		this.Left
-//		this.Right
-//	}
-
+	double Top;
+	double Bottom;
+	double Left;
+	double Right;
+	
+	public Payload(double t, double b, double l, double r){
+		this.Top = t;
+		this.Bottom = b;
+		this.Left = l;
+		this.Right = r;
+	}
+	
+	public double getTop(){
+		return Top;
+	}
+	
+	public void setTop(double t){
+		this.Top = t;
+	}
+	
+	public double getBottom(){
+		return Bottom;
+	}
+	
+	public void setBotton(double b){
+		this.Bottom = b;
+	}
+	
+	public double setLeft(){
+		return Left;
+	}
+	
+	public void setLeft(double l){
+		this.Left = l;
+	}
+	
+	public double getRight(){
+		return Right;
+	}
+	
+	public void setRight(double r){
+		this.Right = r;
+	}
+}			
 
