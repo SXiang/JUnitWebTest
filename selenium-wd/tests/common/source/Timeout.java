6bbf898db41dd57013c99fe48a0b68a9d910a1fe
Shirish Pulikkal <spulikkal@picarro.com>
2017-07-06 15:22:52 -0700

    update start-simulator script to include additional dummy simulator test processes. increase first time load timeout for ci runs.

--- selenium-wd/tests/common/source/Timeout.java
+++ selenium-wd/tests/common/source/Timeout.java
@@ -5,7 +5,7 @@ public class Timeout {
 	public static Integer ANALYZER_READY_DEFAULT_TIMEOUT = 120;
 	public static Integer ANALYZER_READY_LONG_TIMEOUT = 1200;
 
-	public static Integer ANDROID_APP_FIRST_APP_LOAD_TIMEOUT = 90;
+	public static Integer ANDROID_APP_FIRST_APP_LOAD_TIMEOUT = 240;     // very first time if test is running on fresh install, packager can take longer time in CI.
 	public static Integer ANDROID_APP_SCREEN_LOAD_TIMEOUT = 20;
 	public static Integer ANDROID_APP_SHOW_MODAL_TIMEOUT = 5;
 	public static Integer ANDROID_APP_SEARCH_RESULTS_TIMEOUT = 15;
