ab82bf742319b8b3ec0d3440d7c629ec2a3b2d44
Steven Xiang <sxiang@picarro.com>
2017-08-02 16:28:13 -0700

    Added 4 missing EQ surveys to DB Seed script

--- selenium-wd/tests/surveyor/dbseed/source/DbSeedExecutor.java
+++ selenium-wd/tests/surveyor/dbseed/source/DbSeedExecutor.java
@@ -59,7 +59,8 @@ public class DbSeedExecutor {
 			"rr-sqacudr-1-sqacus","rr-sqacudr-2-sqacus","stnd-pic-sqacus", "standard_test-1-sqacus", "standard_test-2-sqacus", "standard_test-3-sqacus", "stnd-sqacudr-sqacus","stnd-sqacudr-1-sqacus",
 			"stnd-sqacudr-2-sqacus","stnd-sqacudr-3-sqacus","StandardWithLeak-sqacus", "NoFOV-1-sqacus", "NoFOV-2-sqacus", "NoFOV-3-sqacus", "daysurvey3.2-1-sqacus", "daysurvey3.2-2-sqacus",
 			"daysurvey4-1-sqacus", "daysurvey5-1-sqacus", "daysurvey7-1-sqacus", "daysurvey8.2-1-sqacus", "daysurvey8-1-sqacus", "daysurvey8-2-sqacus", "AnalyticsTagA-1-sqacus", "AnalyticsTagB-1-sqacus",
-			"AnalyticsTagC-1-sqacus", "FeqNoPeaks-1-sqacus", "FeqWithPeaks-1-sqacus", "MeqNoPeaks-1-sqacus", "MeqWithPeaks-1-sqacus", "FeqWithPeaks02-1-sqacus"};
+			"AnalyticsTagC-1-sqacus", "FeqNoPeaks-1-sqacus", "FeqWithPeaks-1-sqacus", "MeqNoPeaks-1-sqacus", "MeqWithPeaks-1-sqacus", "FeqWithPeaks02-1-sqacus","menlo-night-11-17-EQ-1-sqacus", 
+			"menlo-night-11-17-EQ-2-sqacus","standard-survey-for-EQ-Rpt-1-sqacus", "standard-survey-for-EQ-Rpt-2-sqacus"};
 
 	/* Method to push all the seed data required for automation. */
 
