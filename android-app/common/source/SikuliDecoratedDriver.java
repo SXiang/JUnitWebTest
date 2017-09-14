d9786eccecc9f9c9e19b7c1eb34250487beffb8c
Shirish Pulikkal <spulikkal@picarro.com>
2017-09-14 15:01:46 -0700

    added unit test for sikulidriver image comparison tests

--- android-app/common/source/SikuliDecoratedDriver.java
+++ android-app/common/source/SikuliDecoratedDriver.java
@@ -6,28 +6,31 @@ import java.io.File;
 import org.openqa.selenium.WebDriver;
 import org.sikuli.api.DefaultScreenRegion;
 import org.sikuli.api.ImageTarget;
+import org.sikuli.api.Screen;
 import org.sikuli.api.ScreenRegion;
 
 import androidapp.screens.source.AndroidBaseScreen;
 
 public class SikuliDecoratedDriver {
 	private static final double DEFAULT_SIMILARITY_SCORE = 0.95;
-	private AndroidBaseScreen screen;
+
+	private double similarityScore = DEFAULT_SIMILARITY_SCORE;
+	private Screen screen;
 	private WebDriver driver;
 
-	private SikuliDecoratedDriver(WebDriver driver, AndroidBaseScreen screen) {
+	private SikuliDecoratedDriver(WebDriver driver, Screen screen) {
 		this.driver = driver;
 		this.screen = screen;
 	}
 
-	public static SikuliDecoratedDriver getInstance(WebDriver driver, AndroidBaseScreen screen) {
+	public static SikuliDecoratedDriver getInstance(WebDriver driver, Screen screen) {
 		return new SikuliDecoratedDriver(driver, screen);
 	}
 
 	public Rectangle getImageBounds(File imageFile, Integer waitTimeInSeconds) {
 		Log.method("findImageBounds", imageFile, waitTimeInSeconds);
 		DefaultScreenRegion screenRegion = new DefaultScreenRegion(screen);
-		screenRegion.setScore(DEFAULT_SIMILARITY_SCORE);
+		screenRegion.setScore(this.similarityScore);
 
 		ImageTarget imgTarget = new ImageTarget(imageFile);
 		ScreenRegion foundRegion = screenRegion.wait(imgTarget, waitTimeInSeconds * 1000);
@@ -37,5 +40,9 @@ public class SikuliDecoratedDriver {
 		}
 
 		return null;
+	}
+
+	public void setSimilarityScore(double similarityScore) {
+		this.similarityScore = similarityScore;
 	}
 }
