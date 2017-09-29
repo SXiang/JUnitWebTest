2d9c7e1095beb6bfb681748e431393be723ccc60
Kaminiben Patel <kpatel@picarro.com>
2017-09-29 15:42:10 -0700

    added class for parellal execution in testsuit7

--- selenium-wd/tests/surveyor/testsuite/source/ComplianceReportsTestSuite7.java
+++ selenium-wd/tests/surveyor/testsuite/source/ComplianceReportsTestSuite7.java
@@ -8,13 +8,15 @@ import org.junit.runners.Suite.SuiteClasses;
 import common.source.Log;
 import surveyor.regression.mobile.source.ComplianceReportsInvestigationPageTest2;
 import surveyor.regression.source.ComplianceReportsPageTest12;
+import surveyor.regression.source.ComplianceReportsPageTest_Tahoe;
 import surveyor.scommon.source.SurveyorParallelSuite;
 
 @RunWith(SurveyorParallelSuite.class)
 @SuiteClasses({
 	/* !Don't Put more than one mobile test class in each suite */
 	ComplianceReportsInvestigationPageTest2.class,
-	ComplianceReportsPageTest12.class
+	ComplianceReportsPageTest12.class,
+	ComplianceReportsPageTest_Tahoe.class
 })
 public class ComplianceReportsTestSuite7 {
 
